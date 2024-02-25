f653e76d42010283e3a1e58ef67e1619232672df
Madicke GUEYE <gueyemadicke0751@gmail.com>
2024-02-25 03:18:42 +0100

    Ajout des fonctionnalités : Stop le temps, Gestion de la rotation, Comparaison des diamètres, et ajustements UI pour une meilleure expérience utilisateur

--- src/components/SpaceExplorer.js
+++ src/components/SpaceExplorer.js
@@ -12,6 +12,89 @@ function SpaceExplorer() {
   const [diametersInfo, setDiametersInfo] = useState([]);
   const [rotationSpeed, setRotationSpeed] = useState(1); // Pour la vitesse de rotation
   const [diameterScale, setDiameterScale] = useState(1); // Pour l'échelle des diamètres
+  const [selectedPlanet, setSelectedPlanet] = useState(null);
+  const planetDetails = {
+    Mercure: {
+      type: "Planète rocheuse",
+      taille: "4,879 km",
+      composition: "Roches et métaux",
+      masse: "3.30 × 10^23 kg",
+      distance: "57.9 million km",
+      apparence: "Surface grise criblée de cratères",
+      rotation: "58.6 jours terrestres",
+      orbite: "88 jours terrestres"
+    },
+    Vénus: {
+      type: "Planète rocheuse",
+      taille: "12,104 km",
+      composition: "Atmosphère de dioxyde de carbone avec des nuages d'acide sulfurique",
+      masse: "4.87 × 10^24 kg",
+      distance: "108.2 million km",
+      apparence: "Couvert de nuages jaunes denses",
+      rotation: "243 jours terrestres (rétrograde)",
+      orbite: "225 jours terrestres"
+    },
+    Terre: {
+      type: "Planète rocheuse",
+      taille: "12,742 km",
+      composition: "Azote et oxygène avec une surface d'eau et de terre",
+      masse: "5.97 × 10^24 kg",
+      distance: "149.6 million km",
+      apparence: "Planète bleue avec des océans et des continents",
+      rotation: "24 heures",
+      orbite: "365.25 jours"
+    },
+    Mars: {
+      type: "Planète rocheuse",
+      taille: "6,779 km",
+      composition: "Dioxyde de carbone, azote, et argon",
+      masse: "6.39 × 10^23 kg",
+      distance: "227.9 million km",
+      apparence: "Surface rouge avec des calottes polaires de glace",
+      rotation: "1.03 jours terrestres",
+      orbite: "687 jours terrestres"
+    },
+    Jupiter: {
+      type: "Géante gazeuse",
+      taille: "139,820 km",
+      composition: "Hydrogène et hélium",
+      masse: "1.90 × 10^27 kg",
+      distance: "778.5 million km",
+      apparence: "Planète rayée avec une grande tache rouge",
+      rotation: "9.9 heures",
+      orbite: "11.86 années terrestres"
+    },
+    Saturne: {
+      type: "Géante gazeuse",
+      taille: "116,460 km",
+      composition: "Hydrogène, hélium, et méthane",
+      masse: "5.68 × 10^26 kg",
+      distance: "1.43 milliard km",
+      apparence: "Planète jaune avec des anneaux distinctifs",
+      rotation: "10.7 heures",
+      orbite: "29.5 années terrestres"
+    },
+    Uranus: {
+      type: "Géante de glace",
+      taille: "50,724 km",
+      composition: "Hydrogène, hélium, et eau",
+      masse: "8.68 × 10^25 kg",
+      distance: "2.87 milliard km",
+      apparence: "Planète bleu-vert sans caractéristiques de surface distinctes",
+      rotation: "17.2 heures",
+      orbite: "84 années terrestres"
+    },
+    Neptune: {
+      type: "Géante de glace",
+      taille: "49,244 km",
+      composition: "Hydrogène, hélium, et méthane",
+      masse: "1.02 × 10^26 kg",
+      distance: "4.5 milliard km",
+      apparence: "Planète bleue avec des nuages blancs rapides",
+      rotation: "16 heures",
+      orbite: "164.8 années terrestres"
+    }
+  };
 
   useEffect(() => {
     const scene = new THREE.Scene();
@@ -63,8 +146,8 @@ function SpaceExplorer() {
       const planetInfo = [];
       const loader = new FontLoader();
       loader.load('fonts/Sixty_Regular.json', (font) => {
-        const planetNames = ["Mercure", "Vénus", "Terre", "Mars", "Jupiter", "Saturne", "Uranus", "Neptune", "Pluton"];
-        const planetSpeeds = {"Mercure": 4.74, "Vénus": 3.5, "Terre": 2.98, "Mars": 2.41, "Jupiter": 1.31, "Saturne": 0.97, "Uranus": 0.68, "Neptune": 0.54, "Pluton": 0.47};
+        const planetNames = ["Mercure", "Vénus", "Terre", "Mars", "Jupiter", "Saturne", "Uranus", "Neptune"];
+        const planetSpeeds = {"Mercure": 4.74, "Vénus": 3.5, "Terre": 2.98, "Mars": 2.41, "Jupiter": 1.31, "Saturne": 0.97, "Uranus": 0.68, "Neptune": 0.54};
         const planetDiameters = {
             "Mercure": 4879,
             "Vénus": 12104,
@@ -74,7 +157,6 @@ function SpaceExplorer() {
             "Saturne": 116460,
             "Uranus": 50724,
             "Neptune": 49244,
-            "Pluton": 2376, // Pour l'exemple
           };
         const diametersArray = []; // Pour stocker les informations des diamètres pour l'affichage
 
@@ -101,6 +183,9 @@ function SpaceExplorer() {
         const planetGeometry = new THREE.SphereGeometry(scale, 32, 32);
         const planetMaterial = new THREE.MeshBasicMaterial({ color: THREE.MathUtils.randInt(0x555555, 0xffffff) });
         const planetMesh = new THREE.Mesh(planetGeometry, planetMaterial);
+        // Lors de la création de chaque planète et texte
+        planetMesh.name = `Planet_${name}`;
+    textMesh.name = `Text_${name}`;
         planetMesh.position.set(Math.cos(angle) * radius, Math.sin(angle) * radius, 0);
         scene.add(planetMesh);
       
@@ -143,10 +228,36 @@ function SpaceExplorer() {
 
       renderer.render(scene, camera);
     };
+    
+  const raycaster = new THREE.Raycaster();
+  const mouse = new THREE.Vector2();
+  
+  function onMouseClick(event) {
+    event.preventDefault();
+    
+    mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1;
+    mouse.y = -(event.clientY / renderer.domElement.clientHeight) * 2 + 1;
+    
+    raycaster.setFromCamera(mouse, camera);
+    
+    const intersects = raycaster.intersectObjects(scene.children, true);
+    
+    for (let i = 0; i < intersects.length; i++) {
+        const object = intersects[i].object;
+        if (object.name.startsWith('Planet_') || object.name.startsWith('Text_')) {
+          const planetName = object.name.replace('Planet_', '').replace('Text_', '');
+          setSelectedPlanet(planetName);
+          break;
+        }
+      }
+    }
+  window.addEventListener('click', onMouseClick);
+
       animate();
   
       return () => {
         mountRef.current.removeChild(renderer.domElement);
+        window.removeEventListener('click', onMouseClick);
       };
     }, [isAnimating, rotationSpeed, diameterScale]); // Ajoutez `rotationSpeed` et `diameterScale` aux dépendances
 
@@ -197,9 +308,27 @@ function SpaceExplorer() {
         <p key={index}>{info}</p>
       ))}
     </div>
+    {/* Nouvelle section pour les informations détaillées de la planète sélectionnée */}
+    {selectedPlanet && (
+      <div style={{ position: 'absolute', top: '10%', left: '50%', transform: 'translateX(-50%)', zIndex: 105, color: 'white', backgroundColor: 'rgba(0, 0, 0, 0.7)', padding: '20px', borderRadius: '10px', width: '300px', textAlign: 'center' }}>
+        <h2>{selectedPlanet}</h2>
+        {planetDetails[selectedPlanet] && (
+          <>
+            <p>Type: {planetDetails[selectedPlanet].type}</p>
+            <p>Taille: {planetDetails[selectedPlanet].taille}</p>
+            <p>Composition: {planetDetails[selectedPlanet].composition}</p>
+            <p>Masse: {planetDetails[selectedPlanet].masse}</p>
+            <p>Distance: {planetDetails[selectedPlanet].distance}</p>
+            <p>Apparence: {planetDetails[selectedPlanet].apparence}</p>
+            <p>Orbite: {planetDetails[selectedPlanet].orbite}</p>
+          </>
+        )}
+      </div>
+    )}
   </div>
 );
     }
     
 
 export default SpaceExplorer;
+
